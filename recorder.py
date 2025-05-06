6b3c154ce5b0b31d74e05b7d8f2fe33d002ef240
VuLe005 <157918086+VuLe005@users.noreply.github.com>
2025-05-05 18:55:10 -0700

    test

--- recorder.py
+++ recorder.py
@@ -116,3 +116,5 @@ def run_brainflow():
 
     df_brainflow = pd.DataFrame(eeg.T, columns=[f"EEG_Channel_{i}" for i in range(eeg.shape[0])])
     df_brainflow.to_csv(os.path.join(save_dir, f'EEG_run-{run}.csv'), index=False)
+
+    run_brainflow
